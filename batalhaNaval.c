85a8771d057337fcaabb7d3d6bcd05834144c6c6
Antonio Carlos Barros Ferreira <xdatabycarlos@gmail.com>
2025-10-05 12:24:15 -0300

    Código Nivel Mestre batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -18,56 +18,95 @@ int main(){
         }        
     }
 
-    //02. Posicionamento dos Navios
 
+    // 02. Posicionamento das peças:
     /*
-    Tipo de Navio	Tamanho	Quantidade
-    Porta-Aviões	5       1
-    Encouraçado	    4	    1
-    Cruzador	    3	    2
-    Submarino	    2	    1
- 
+    
+    Navio Vertical     = Numero do tabuleiro -> 3
+    Cone               = Numero do tabuleiro -> 4
+    Cruz               = Numero do tabuleiro -> 5
+    Octaedro           = Numero do tabuleiro -> 6
+    Navio Horizontal   = Numero do tabuleiro -> 7
+    Navio Diagonanal 1 = Numero do tabuleiro -> 8
+    Navio Diagonanal 2 = Numero do tabuleiro -> 9
+
+
+    
     */
-
-    // Posicionamento do Porta-Aviões (Vertical)
-    // Linha 03 até 07 - Coluna E
-
-    for (int i = 2; i < 7; i++)
+    for (int i = 4; i < 9; i++)
     {
-        tabuleiro[i][8] = 3;
+        tabuleiro[i][0] = 3; // Linha 5 (i = 4) até Linha 9 (i = 8) na coluna A (j = 0)
     }
 
-    // Posicionamento do Encouraçado (Horizontal)
-    // Linha 08 - Coluna C até F
 
-    for (int i = 2; i < 6; i++)
+    // Navio Horizontal
+    for (int j = 2; j < 6; j++) 
     {
-        tabuleiro[8][i] = 3;
+        tabuleiro[4][j] = 7; // Linha 5 (i = 4) e Coluna de C até a F
     }
 
-    // Encouraçado na Diagonal principal
-    for (int i = 0; i < 4; i++)
+    // Cone
+    for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < 5; j++)
         {
-            if (i == j) // Mesmo indice de linhas e colunas
-            {
-                tabuleiro[i][j] = 3;
+            if (j >= 2-i && j <= 2+i) // Se i for 1, j vai ser entre>= 2 - 1  e <= 2 + 1
+            {                         // de j = 1 a j = 3 
+                tabuleiro[i][j] = 4;
             }
-            
-        }
-        
+        }    
     }
 
-    // Encouraçado na Diagonal secundaria
+    // Cruz
+    for (int i = 7; i <= 9; i++)
+    {
+        for (int j = 5; j <= 9; j++)
+        {
+            if (i == 8 || j == 7) // Sempre que for a linha 8 ou a coluna 7 (H)
+            {
+                tabuleiro[i][j] = 5;
+            }      
+        }    
+    }
+
+    // Diagonal Principal
     for (int i = 3; i < 7; i++)
     {
+        // j = i + 3 
+        tabuleiro[i][i+3] = 8; // Ex.: i = 6, então j = 6 + 3 (9)       
+    }
+
+    // Diagonal Secundária
+    for (int i = 5; i < 9; i++)
+    {
         // j = 9 - i
-        tabuleiro[i][9-i] = 3; // Ex.: i = 6, então j = 9 - 6 (3)
+        tabuleiro[i][9-i] = 9; // Ex.: i = 5, então j = 9 - 5 (4)
     }
     
-    
-    // 03. Impressão do Tabuleiro
+
+    // Octaedro
+
+    int raio_max = 1;
+    int centro_j = 7;
+    int centro_i = 1;
+
+    /*
+    i = 1 (centro_i) - 1 (raio_max) (0) até i = 1 + 1 (2)
+    j = 7 (centro_j) - 1 (raio_max) (6 ou G)  até j = 7 + 1 (8 ou J)     
+    */ 
+    for (int i = centro_i - raio_max; i <= centro_i + raio_max; i++)
+    {
+        for (int j = centro_j - raio_max; j <= centro_j + raio_max; j++)
+        {
+            if (i == 1 || j == 7) // Sempre que for a linha 2 (i = 1) ou a coluna 7 (H)
+            {
+                tabuleiro[i][j] = 6;
+            }       
+            
+        }    
+    }
+
+    // 02. Impressão do Tabuleiro
 
     printf("------ BATALHA NAVAL ------\n\n");
 
@@ -93,13 +132,5 @@ int main(){
         printf("\n");
     }
     
-    
-    
-    
-
-
-
-
-
     return 0;
 }
